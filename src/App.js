fb7f90bc48e97191c6d050496bc4a1f1e1100c8e
Adnan Khan <adnan7.zain@gmail.com>
2021-11-21 22:58:14 +0500

    create the form and get the values'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPerson = { name, age, email };
+    setPeople([...people, newPerson]);
+    console.log(newPerson);
+    setName('');
+    setAge('');
+    setEmail('');
+  };
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <section className='container'>
+        <h4>Add Person to the list</h4>
+
+        <form className='form'>
+          <div className='form-control'>
+            <label htmlFor='name'>Name :</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className='form-control'>
+            <label htmlFor='age'>Age :</label>
+            <input
+              type='number'
+              name='age'
+              id='age'
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+
+          <div className='form-control'>
+            <label htmlFor='email'>Email :</label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type='submit' onClick={handleSubmit}>
+            Add Person
+          </button>
+        </form>
+      </section>
+    </>
   );
 }
 
